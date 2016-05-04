54a8a339b07e96d282b20da988f2764b37ed54c4
LisaVuong <hello.lisavuong@gmail.com>
2016-05-04 12:24:51 -0500

    mobile menu almost works

--- js/navigation.js
+++ js/navigation.js
@@ -7,3 +7,13 @@ $(window).scroll(function() {
         $(".nav-bar").removeClass("scrolling");
     }
 });
+
+$( ".cd-nav-trigger" ).click(function() {
+	if ($('.nav-bar').hasClass('is-visible')) {
+		$(".nav-bar").removeClass("is-visible");
+		$('.cd-nav-trigger').html('Majors');
+	} else {
+		$(".nav-bar").addClass("is-visible");
+		$('.cd-nav-trigger').html('Close');
+	}
+});
